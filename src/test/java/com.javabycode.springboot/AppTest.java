9d19ad391bc1cbc88dd09cfbe52e6f2b7c5bc4a9
d.messina <d.messina@wavenet.be>
2019-10-28 14:59:03 +0100

    Added one test

---
+++ src/test/java/com.javabycode.springboot/AppTest.java
@@ -0,0 +1,13 @@
+package com.javabycode.springboot;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void apply() {
+        Assertions.assertThat("Click me to say Hello").contains("Hello");
+    }
+}
