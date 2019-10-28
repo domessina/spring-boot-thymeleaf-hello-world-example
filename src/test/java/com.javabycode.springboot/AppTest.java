e33fa4d51d9ca481eeb50d56c437a422371356a5
d.messina <d.messina@wavenet.be>
2019-10-28 15:50:57 +0100

    test git hook 6

--- src/test/java/com.javabycode.springboot/AppTest.java
+++ src/test/java/com.javabycode.springboot/AppTest.java
@@ -9,6 +9,5 @@ public class AppTest {
     @Test
     public void apply() {
         Assertions.assertThat("Click me to say Hello").contains("Hello");
-        System.out.println("sel :D");
     }
 }
